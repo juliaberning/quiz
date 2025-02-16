51cd57c3f964e7c96a413ac1501abd47281f50d5
Julia <julia.berning@thenotcompany.com>
2025-02-16 14:35:10 +0100

    feat: add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,97 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
-import { onMounted } from 'vue'
-
-onMounted(async () => {
-  try {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-    console.log('Axios response:', response.data)
-  } catch (error) {
-    console.error('Axios error:', error)
-  }
-})
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="min-h-screen bg-gray-100"> 
+    <NavBar class="bg-white text-gray-900 shadow-md w-full" /> 
+    <div class="px-4 py-8 sm:px-6 lg:px-8">
+      <RouterView />
     </div>
-  </header>
-
-  <RouterView />
-  <div class="text-4xl font-bold text-blue-500">This app is using Tailwind!</div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
