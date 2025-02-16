7b29ff995ab91cf826453721068a4d395a267102
Julia <julia.berning@thenotcompany.com>
2025-02-16 12:29:26 +0100

    chore: add axios

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import { onMounted } from 'vue'
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    console.log('Axios response:', response.data)
+  } catch (error) {
+    console.error('Axios error:', error)
+  }
+})
 </script>
 
 <template>
