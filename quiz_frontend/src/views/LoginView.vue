813cc0fef2f0f682c676cd34381be615f98ededc
Julia <julia.berning@thenotcompany.com>
2025-02-24 12:29:01 +0100

    feat: add login to frontend

--- quiz_frontend/src/views/LoginView.vue
+++ quiz_frontend/src/views/LoginView.vue
@@ -1,12 +1,4 @@
 <template>
-    <!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
     <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company">
@@ -14,13 +6,13 @@
   </div>
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-    <form class="space-y-6" action="#" method="POST">
+    <form class="space-y-6" @submit.prevent="submitForm">
 
-      <!-- Name Field -->
+      <!-- Email Field -->
       <div>
         <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
         <div class="mt-2">
-          <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+          <input type="email" v-model="form.email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
         </div>
       </div>
 
@@ -33,9 +25,15 @@
           </div>
         </div>
         <div class="mt-2">
-          <input type="password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
+          <input type="password" v-model="form.password" name="password" id="password" autocomplete="current-password" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
         </div>
       </div>
+
+      <template v-if="errors">
+        <div class="bg-red-300 text-white rounded-lg p-6">
+          <p v-for="error in errors" v-bind:key="error"> {{ error }} </p>
+        </div>
+      </template>
 
       <!-- Submit Button -->
       <div>
@@ -53,3 +51,70 @@
 </div>
 
 </template>
+
+<script>
+import axios from 'axios'
+
+import { useUserStore } from '@/stores/user'
+
+export default {
+setup() {
+    const userStore = useUserStore()
+
+    return {
+        userStore
+    }
+},
+
+data() {
+    return {
+        form: {
+            email: '',
+            password: '',
+        },
+        errors: []
+    }
+},
+methods: {
+  async submitForm() {
+    this.errors = []
+
+    if (this.form.email === '') {
+      this.errors.push('Your e-mail is missing')
+    }
+
+    if (this.form.password === '') {
+      this.errors.push('Your password is missing')
+    }
+
+    if (this.errors.length === 0) {
+      await axios
+          .post('/api/login/', this.form)
+          .then(response => {
+              this.userStore.setToken(response.data)
+
+              axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.access;
+          })
+          .catch(error => {
+              console.log('error', error)
+
+              this.errors.push('The email or password is incorrect! Or the user is not activated!')
+          })
+    }
+    
+    if (this.errors.length === 0) {
+      await axios
+          .get('/api/me/')
+          .then(response => {
+              this.userStore.setUserInfo(response.data)
+
+              this.$router.push('/')
+          })
+          .catch(error => {
+              console.log('error', error)
+          })
+    }
+  }
+}
+}
+</script>
